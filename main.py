19ea7baf76c880a76debfc13510e41131b84c627
t0491 <thanhoang444@gmail.com>
2021-12-24 17:42:44 -0800

    clarity update
    turned redundant code into functions to be called, e.g. play_round(). reordered function listing for better read flow. renamed class functions to be consistent, e.g. "show" and "get" were the same and now both under "get". moved the transferring of a player's cd to bd into the draw function since they naturally go hand in hand in the event that the player has no more cards in their bd to draw from. created a description at the top for users/reviewers to understand what the program is about.

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-# Simulate a card battle between two players.
-# Shuffle the deck and split it in half.
-# Each player will have half of the deck respectively as their "battle deck".
-# Players will then draw the top card of their battle deck, play it, and then compare results.
-# Player that played the higher value card will then keep both of the played cards as score under the "captured deck".
+# Simulate a card battle between N players.
+# User may choose to play vs. bots or watch bots play each other.
+# Shuffle the deck and split it into N equal parts.
+# Each player will receive their split of the deck as their "battle deck".
+# Players will then draw up to 5 cards into their hand depending on the game mode.
+# Players will simultaneously choose which card to play from their hand depending on their AI.
+# The player that played the highest value card will then capture all played cards as score under the "captured deck".
+# This "captured deck" will become the player's "battle deck" in the event that their battle deck is empty.
+# In the event of a tie, the players will repeat the draw and play phase until there is one clear winner.
+# If any player runs out of cards they are eliminated from the game.
+# The last remaining player (should have possession of all the cards, N * 12 cards) is the winner.
 
 from typing import Dict, List
 import numpy
@@ -10,28 +16,35 @@ import random
 
 class Player: 
     # Init for the Player
-    def __init__(self, my_id, my_deck, bot_ai) -> None:
+    def __init__(self, my_id, battle_deck, bot_ai) -> None:
         self.my_id = my_id # Used for identifying which player this is in a BR scenario once others are elim'd/deleted.
-        self.my_deck = my_deck # Contains the person's battle deck.
-        self.bot_ai = bot_ai # Configured for random or high value priority.
-        self.my_hand = [] # Holds the players drawn cards before playing.
-        # Set up by having each player draw 5 cards in their hand at first if it's not top-deck.
-        if self.bot_ai != 1:
-            for _ in range(5):
-                self.draw_card()
-        # Otherwise it doesn't matter and they'll play top-deck.
-        else:
-            self.draw_card()
+        self.battle_deck = battle_deck # Contains the person's battle deck.
         self.captured_deck = [] # Points/captured cards. Will be used as battle deck once that is empty.
+        self.my_hand = [] # Holds the players drawn cards before playing.
         self.played_card = None
+        self.bot_ai = bot_ai # Configured for random or high value priority.
+        # Set up by having each player draw 4 cards in their hand at first if it's not top-deck.
+        if self.bot_ai != 1:
+            for _ in range(4):
+                self.draw_card()
+        # Otherwise they'll draw their top-deck card in start_sim()
+
+    def get_id(self) -> int:
+        return self.my_id
 
     # Returns the player's battle deck.
     def get_bd(self) -> list:
-        return self.my_deck
+        return self.battle_deck
 
     # Return the player's captured deck.
     def get_cd(self) -> list:
         return self.captured_deck
+
+    def get_hand(self) -> list:
+        return self.my_hand
+        
+    def get_played_card(self) -> int:
+        return self.played_card
 
     def get_ai(self) -> str:
         if self.bot_ai == 1:
@@ -43,12 +56,24 @@ class Player:
         elif self.bot_ai == None:
             return "Real Player"
 
+    # Copy over all of the captured cards to battle and then clear the captured.
+    def transfer_cd_to_bd(self) -> None:
+        self.battle_deck = self.captured_deck.copy()
+        self.captured_deck.clear()
+
     # Takes the top card out of their battle deck and adds it into their hand.
     def draw_card(self) -> None:
-        self.my_hand.append(self.my_deck.pop())
+        # If their battle deck is empty but they have captured cards, turn it into their battle cards.
+        if len(self.battle_deck) == 0 and len(self.captured_deck) > 0:
+            self.transfer_cd_to_bd()
+            deck_shuffle(self.battle_deck) # Shuffle it afterwards.
+
+        # You can only draw if you have cards left in your battle deck.
+        if len(self.battle_deck) > 0:
+            self.my_hand.append(self.battle_deck.pop())
     
     # Sets the inputted card as the player's played_card depending on the AI.
-    def play_card(self) -> int:
+    def play_card(self) -> None:
         if self.bot_ai == 1: # Top-deck AI
             self.played_card = self.my_hand.pop(0) # Remove the first card in hand.
         elif self.bot_ai == 2: # Random AI
@@ -84,70 +109,6 @@ class Player:
     # Allocates all of the round winnings into the player's win pile/captured deck.
     def capture_card(self, card: int) -> None:
         self.captured_deck.append(card)
-
-    def show_played_card(self) -> int:
-        return self.played_card
-
-    def show_hand(self) -> list:
-        return self.my_hand
-
-    def show_id(self) -> int:
-        return self.my_id
-
-    # Copy over all of the captured cards to battle and then clear the captured.
-    def transfer_cd_to_bd(self) -> int:
-        self.my_deck = self.captured_deck.copy()
-        self.captured_deck.clear()
-
-def main() -> None:
-    
-    # Determines if the user wants player vs AI or have a simulated AI vs AI
-    game_mode = acquire_game_mode()
-
-    # Prompts user for # of playersbo to setup the game.
-    num_bots = acquire_bots(game_mode)
-
-    # Generates the deck based on # of players + shuffles it.
-    if game_mode == 1:
-        total_players = num_bots + 1 # Plus the user themself.
-    elif game_mode == 2:
-        total_players = num_bots
-    
-    # Determines if we are doing top-deck or strategic.
-    ai_style = []
-    ai_type = acquire_ai_type()
-
-    # Top-deck(1), then there is no AI choice pattern.
-    if ai_type == 1:
-        for i in range(total_players):
-            ai_style.append(1)
-    # Strategic, we allocate them as Random(2) or Highest Value(3) then.
-    elif ai_type == 2:
-        for i in range(total_players):
-            ai_style.append(random.randint(2,3))
-
-    game_deck = create_deck(total_players)
-    
-    # Split the deck into equal parts for the players.
-    split_deck = numpy.array_split(game_deck, total_players)
-
-    # Initialize a dictionary containing all players, we Key them with their IDs.
-    player_list = {}
-
-    # Give them their decks as well. User player will be player_list[1].
-    for i in range(total_players):
-        if game_mode == 1 and i == 0: # Init the user player specifically for a bot_ai of "None"
-            player_list[i+1] = Player(i+1, split_deck[i].tolist(), None) 
-        else: # Else init all bots
-            player_list[i+1] = Player(i+1, split_deck[i].tolist(), ai_style[i]) 
-
-    """If you want to check what AI each bot has to check if their decisions are correct."""
-    #for player in player_list.values():
-        #print("Player #" + str(player.show_id()) + "'s AI is " + player.get_ai())
-
-    # Begin running the battle simulation.
-    start_simulation(player_list)
-    return
 
 def acquire_game_mode() -> int:
     game_mode = input("What type of game mode would you like? \n1: Play vs. Bots \n"
@@ -211,6 +172,76 @@ def deck_shuffle(deck: List[int]) -> list:
     # Do the shuffling here. Move the contents of the list/array randomly within itself.
     return numpy.random.shuffle(deck)
 
+def eliminate_empty_players(player_list: Dict) -> None:
+    # Check player deck/resources now. If they are 0, then the player is eliminated.
+    to_be_elimd = []
+    for player in player_list.values():
+        # If both decks and hand are empty, eliminate them.
+        if len(player.get_bd()) == 0 and len(player.get_cd()) == 0 and len(player.get_hand()) == 0:
+            to_be_elimd.append(player.get_id())
+
+    for i in to_be_elimd:
+        player_list.pop(i)
+    
+    return
+
+def play_round(player: Player, highest_card: int, round_winner: List[int], pooled_cards: List[int]) -> int:
+    # Draw and play the card here.
+    player.draw_card()
+
+    # Real Players get to choose their card in here, refer to .play_card() definition.
+    player.play_card() # Play the card according to their AI
+
+    # print the hand below if you want to check and see if the AI is playing correctly by their "bot_ai"
+    # print("Player #" + str(player.get_id() + "'s hand: " + player.get_hand())
+    print("Player #" + str(player.get_id()) + " plays, " + str(player.get_played_card()) + "!")
+
+    # Compare the played card results here.
+    # Update the new highest card.
+    if player.get_played_card() > highest_card:
+        highest_card = player.get_played_card()
+        round_winner.clear() # Clear the array of all previous winners/ties if there's a new higher value.
+        round_winner.append(player.get_id()) # Add the updated winner to the list.
+
+    elif player.get_played_card() == highest_card:
+        round_winner.append(player.get_id()) # Add another winner into the list for a tie.
+
+    # Everyone pools their cards into the middle per play.
+    pooled_cards.append(player.get_played_card())
+
+    return highest_card
+
+# Returns the ID of the tie winner, may recurse.
+# If it does recurse, the new inputted round_winner will dwindle down and remove any losers of the tiebreaker.
+def exe_tiebreaker(player_list: Dict, old_round_winner: List[int], pooled_cards: List[int]) -> list:
+    
+    # Removes any players with no cards remaining.
+    eliminate_empty_players(player_list)
+
+    highest_card = 0
+    round_winner = []
+    # End the tiebreaker phase if there are no other players to play.
+    # Return the last remaining player as the round_winner
+    if len(player_list) < 2:
+        for player in player_list.values():
+            round_winner.append(player)
+        return round_winner
+
+    print("### TIE BREAKER ###")
+
+    for player in player_list.values():
+        # Only the winners of the previous round (the ones that tied) will be playing now.
+        if player.get_id() in old_round_winner:
+            highest_card = play_round(player, highest_card, round_winner, pooled_cards)
+
+    #print("Remaining Players: " + str(len(player_list)))
+
+    if len(round_winner) > 1:
+        round_winner = exe_tiebreaker(player_list, round_winner, pooled_cards)
+
+    # In order for the recursion to stop, the returned round winner must be a size of 1.
+    return round_winner
+
 def start_simulation(player_list: Dict) -> None:
 
     round_counter = 1
@@ -233,34 +264,9 @@ def start_simulation(player_list: Dict) -> None:
         # Keeps track of current highest value card.
         highest_card = 0
 
-        # Draw and play the card here.
+        # Each player will go through the play phase of the game.
         for player in player_list.values():
-
-            # Real Players get to choose their card in here, refer to .play_card() definition.
-            player.play_card() # Play the card according to their AI
-
-            # Only draw if they have cards left in their battle deck.
-            # If they don't then they should still have cards left in their hand to play.
-            # They would have been eliminated if their hand, bd, and cd were all empty.
-            if len(player.get_bd()) > 0:
-                player.draw_card()
-
-            # print the hand below if you want to check and see if the AI is playing correctly by their "bot_ai"
-            # print("Player #" + str(player.show_id() + "'s hand: " + player.show_hand())
-            print("Player #" + str(player.show_id()) + " plays, " + str(player.show_played_card()) + "!")
-
-            # Compare the played card results here.
-            # Update the new highest card.
-            if player.show_played_card() > highest_card:
-                highest_card = player.show_played_card()
-                round_winner.clear() # Clear the array of all previous winners/ties if there's a new higher value.
-                round_winner.append(player.show_id()) # Add the updated winner to the list.
-
-            elif player.show_played_card() == highest_card:
-                round_winner.append(player.show_id()) # Add another winner into the list for a tie.
-
-            # Everyone pools their cards into the middle per play.
-            pooled_cards.append(player.show_played_card())
+            highest_card = play_round(player, highest_card, round_winner, pooled_cards)
 
         if len(round_winner) > 1:
             # Execute the tiebreaker here, perhaps recurses until the tie finally resolves.
@@ -268,7 +274,7 @@ def start_simulation(player_list: Dict) -> None:
 
         # Checks to see current winnings so far.
         print("Pooled Cards: " + str(pooled_cards))
-        print("Round Winner: " + str(round_winner[0]))
+        print("Round Winner: Player #" + str(round_winner[0]))
 
         # Give the winner all the pooled cards.
         for card in pooled_cards:
@@ -284,77 +290,59 @@ def start_simulation(player_list: Dict) -> None:
     # Should only print once for the last remaining player.
     for player in player_list.values():
         print("### GAME OVER ###")
-        print("Player #" + str(player.show_id()) + " is the winner of Card Battle Royale!")
+        print("Player #" + str(player.get_id()) + " is the winner of Card Battle Royale!")
 
         # Should total up to 12 * num_players.
-        print("They have collected all " + str(len(player.get_bd()) + len(player.get_cd()) + len(player.show_hand())) + " cards!")
+        print("They have collected all " + str(len(player.get_bd()) + len(player.get_cd()) + len(player.get_hand())) + " cards!")
     return
 
-# Returns the ID of the tie winner, may recurse.
-# If it does recurse, the new inputted round_winner will dwindle down and remove any losers of the tiebreaker.
-def exe_tiebreaker(player_list: Dict, old_round_winner: List[int], pooled_cards: List[int]) -> list:
+def main() -> None:
     
-    # Removes any players with no cards remaining.
-    eliminate_empty_players(player_list)
+    # Determines if the user wants player vs AI or have a simulated AI vs AI
+    game_mode = acquire_game_mode()
 
-    highest_card = 0
-    round_winner = []
-    # End the tiebreaker phase if there are no other players to play.
-    # Return the last remaining player as the round_winner
-    if len(player_list) < 2:
-        for player in player_list.values():
-            round_winner.append()
-        return round_winner
+    # Prompts user for # of playersbo to setup the game.
+    num_bots = acquire_bots(game_mode)
 
-    print("### TIE BREAKER ###")
-    for player in player_list.values():
-        #print(len(player_list))
-        if player.show_id() in old_round_winner:
-
-            # Draw and play the card here.
-            player.play_card()
-            # Once again, only draw if there are cards left in bd.
-            if len(player.get_bd()) > 0:
-                player.draw_card()
-        
-            # Compare the played card results here.
-            print("Player #" + str(player.show_id()) + " plays, " + str(player.show_played_card()) + "!")
-            # Update the new highest card.
-            if player.show_played_card() > highest_card:
-                highest_card = player.show_played_card()
-                round_winner.clear() # Clear the array of all previous winners/ties if there's a new higher value.
-                round_winner.append(player.show_id()) # Add the updated winner to the list.
-
-            elif player.show_played_card() == highest_card:
-                round_winner.append(player.show_id()) # Add another winner into the list for a tie.
-
-            pooled_cards.append(player.show_played_card())
-
-    #print("Remaining Players: " + str(len(player_list)))
-
-    if len(round_winner) > 1:
-        round_winner = exe_tiebreaker(player_list, round_winner, pooled_cards)
-
-    # In order for the recursion to stop, the returned round winner must be a size of 1.
-    return round_winner
-
-def eliminate_empty_players(player_list: Dict) -> None:
-    # Check player deck/resources now. If they are 0, then the player is eliminated.
-    to_be_elimd = []
-    for player in player_list.values():
-        # If they have captured cards, turn it into their battle cards.
-        if len(player.get_bd()) == 0 and len(player.get_cd()) > 0:
-            player.transfer_cd_to_bd()
-            deck_shuffle(player.get_bd()) # Shuffle it afterwards.
-        # If they still have cards in their battle deck or in their hand, they aren't eliminated.
-        elif len(player.get_bd()) > 0 or len(player.show_hand()) > 0:
-            continue
-        # If both decks empty, eliminate. Should only end up here if their hand is empty as well.
-        elif len(player.get_bd()) == 0 and len(player.get_cd()) == 0:
-            to_be_elimd.append(player.show_id())
-
-    for i in to_be_elimd:
-        player_list.pop(i)
+    # Generates the deck based on # of players + shuffles it.
+    if game_mode == 1:
+        total_players = num_bots + 1 # Plus the user themself.
+    elif game_mode == 2:
+        total_players = num_bots
     
+    # Determines if we are doing top-deck or strategic.
+    ai_style = []
+    ai_type = acquire_ai_type()
+
+    # Top-deck(1), then there is no AI choice pattern.
+    if ai_type == 1:
+        for i in range(total_players):
+            ai_style.append(1)
+    # Strategic, we allocate them as Random(2) or Highest Value(3) then.
+    elif ai_type == 2:
+        for i in range(total_players):
+            ai_style.append(random.randint(2,3))
+
+    game_deck = create_deck(total_players)
+    
+    # Split the deck into equal parts for the players.
+    split_deck = numpy.array_split(game_deck, total_players)
+
+    # Initialize a dictionary containing all players, we Key them with their IDs.
+    player_list = {}
+
+    # Give them their decks as well. User player will be player_list[1].
+    for i in range(total_players):
+        if game_mode == 1 and i == 0: # Init the user player specifically for a bot_ai of "None"
+            player_list[i+1] = Player(i+1, split_deck[i].tolist(), None) 
+        else: # Else init all bots
+            player_list[i+1] = Player(i+1, split_deck[i].tolist(), ai_style[i]) 
+
+    """If you want to check what AI each bot has to check if their decisions are correct."""
+    #for player in player_list.values():
+        #print("Player #" + str(player.get_id()) + "'s AI is " + player.get_ai())
+
+    # Begin running the battle simulation.
+    start_simulation(player_list)
     return
 main()
